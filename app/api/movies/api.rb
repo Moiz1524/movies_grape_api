633e98c343e0430138ffc27dd0561db19d5da41b
Moiz Ali <amoiz4142@gmail.com>
2020-04-29 00:16:01 +0500

    Implemented config and Grape API RESTful actions.

---
+++ app/api/movies/api.rb
@@ -0,0 +1,66 @@
+module Movies
+  class API < Grape::API
+    format :json
+    prefix :api
+    version 'v1', :path
+
+    resources :movies do
+      # GET /api/v1/movies
+      desc 'It returns all movies'
+      get { Movie.all }
+
+      # GET /api/v1/movies/:id
+      desc 'Return specific movie'
+      route_param :id, type: Integer do
+        get do
+          movie = Movie.find(params[:id])
+          { movie: movie }
+        rescue ActiveRecord::RecordNotFound
+          error!('Record Not Found', 404) 
+        end
+      end
+
+      # POST /api/v1/movies
+      desc 'Creates a movie object'
+      params do
+        requires :name, type: String
+        requires :released, type: DateTime
+        optional :points, type: Integer
+        optional :liked, type: Boolean
+      end
+      post do
+        movie = Movie.create!(declared(params))
+        { movie: movie, message: 'Movie created successfully.' }
+      end
+
+      # PUT/PATCH api/v1/movie/:id
+      desc 'Update existing movie object'
+      params do
+        requires :name, type: String
+        requires :released, type: DateTime
+        optional :points, type: Integer
+        optional :liked, type: Boolean
+      end
+      route_param :id do
+        put do
+          movie = Movie.find(params[:id])
+          movie.update(declared(params))
+          { message: 'Movie updated successfully.'}
+        rescue ActiveRecord::RecordNotFound
+          error!('RecordNotFound', 404)
+        end
+      end
+
+      # DELETE api/v1/movie/:id
+      desc 'Deletes existing movie object'
+      route_param :id, type: Integer do
+        delete do
+          Movie.find(params[:id]).delete
+          { message: 'Movie deleted successfully.' }
+        rescue ActiveRecord::RecordNotFound
+          error!('Record Not Found', 404)
+        end
+      end
+    end
+  end
+end
